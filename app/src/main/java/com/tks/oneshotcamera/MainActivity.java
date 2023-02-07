09795ece0f9500208724fc5ad28bfb487301a4ff
aaaaa <aaaaa>
2023-02-07 20:38:21 +0900

    作成中

--- app/src/main/java/com/tks/oneshotcamera/MainActivity.java
+++ app/src/main/java/com/tks/oneshotcamera/MainActivity.java
@@ -10,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (null == savedInstanceState) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commit();
+        }
     }
 }
